f675f6f7af462dc5cc04861bed9efae2267bc7e0
Darun Karas <darunkaras@Daruns-MacBook-Air.local>
2024-12-26 04:53:56 +0600

    login and log out using jwt

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ require('dotenv').config()
 const app = express()
 //middleware
 app.use(cors({
-    origin: ['http://localhost:5173'
+    origin: ['http://localhost:5173',
+        'https://blog-app-d54d1.web.app',
+        'https://console.firebase.google.com/project/blog-app-d54d1/overview',
+        'https://blog-app-d54d1.firebaseapp.com'
     ],
     credentials: true,
 }));
@@ -56,7 +59,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         //data base
         const database = client.db('LetsBlogDB')
@@ -79,6 +82,16 @@ async function run() {
                 .send({ success: true });
         })
 
+        //logout
+        app.post('/logout', (req, res) => {
+            res
+                .clearCookie('token', {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV === 'production',
+                    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+                })
+                .send({ success: true })
+        })
         //creating index for enabling search
         await blogCollections.createIndex({ title: "text", description: "text" });
         //Blog Routes
@@ -94,8 +107,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
